7810b68c3d95c394f46436bd5ee134cfeee1e57e
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2021-12-10 08:16:27 -0600

    added tests and comments as well as cleaned up code

--- src/App.test.js
+++ src/App.test.js
@@ -1,82 +1,47 @@
 import App from "./App";
-//import ReactDOM from "react-dom";
-//import { act } from "react-dom/test-utils";
 import React from "react";
-//import Adapter from "enzyme-adapter-react-17";
-import { shallow, configure } from "enzyme";
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+import { shallow } from "enzyme";
 import "@testing-library/jest-dom";
 import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
-// let container;
-
-// //With beforeEach will create a new div for each test in the file and adding it.
-// beforeEach(() => {
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//   document.body.removeChild(container);
-//   container = null;
-// });
-
-// it("confirms the textInput field contains the default placeholder", () => {
-//   act(() => {
-//     ReactDOM.render(<App />, container);
-//   });
-//   const defaultTextInput = container.querySelector("textarea");
-//   expect(defaultTextInput.value).toBe("Here is some example text.");
-// });
-
-//test the radio buttons to see if state changes
-// it("confirms the textInput field contains the default placeholder and nothing else", () => {
-//   act(() => {
-//     ReactDOM.render(<App />, container);
-//   });
-//   const defaultTextInput = container.querySelector("textarea");
-//   expect(defaultTextInput.value).not.toBe("Here is some example text.");
-// });
 
 describe("App", () => {
+  const setConversionMode = jest.fn();
+  const handleClick = jest.spyOn(React, "useState");
+  const handleSubmit = jest.fn();
+
+  const wrapper = shallow(
+    <App onChange={setConversionMode} handleSubmit={handleSubmit} />
+  );
+
+  //click handler for radio buttons converting between lowercase and uppercase
+  handleClick.mockImplementation((conversionMode) => [
+    conversionMode,
+    setConversionMode,
+  ]);
+
+  //TEST 1: CHECKING IF THE APP RENDERS CORECTLY
   it("renders correctly", () => {
-    shallow(<App />);
+    wrapper.find(<App />);
   });
 
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
-
-  it("should update state on click for lowercase", () => {
-    const setConversionMode = jest.fn();
-    const wrapper = shallow(<App onClick={setConversionMode} />);
-    const handleClick = jest.spyOn(React, "useState");
-    handleClick.mockImplementation((conversionMode) => [
-      conversionMode,
-      setConversionMode,
-    ]);
-
+  //TEST 2: CHECKING IF THE LOWERCASE RADIO BUTTON FUNCTIONS CORRECTLY BY CHANGING STATE
+  it("should update lowercase state on click", () => {
     wrapper.find("#conversion-0").simulate("click");
     expect(setConversionMode).toBeTruthy();
   });
 
-  it("should update state on click for uppercase", () => {
-    const setConversionMode = jest.fn();
-    const wrapper = shallow(<App onClick={setConversionMode} />);
-    const handleClick = jest.spyOn(React, "useState");
-    handleClick.mockImplementation((conversionMode) => [
-      conversionMode,
-      setConversionMode,
-    ]);
-
+  //TEST 3: CHECKING IF THE UPPERCASE RADIO BUTTON FUNCTIONS CORRECTLY BY CHANGING STATE
+  it("should handle uppercase radio change onclick", () => {
     wrapper.find("#conversion-1").simulate("click");
     expect(setConversionMode).toBeTruthy();
   });
+
+  //TEST 4: CHECKING IF SUBMIT BUTTON FUNCTIONS CORRECTLY BY CALLING HANDLESUBMIT
+  it("should confirm submit button clicked", () => {
+    wrapper.find("#submit").simulate("click");
+    expect(handleSubmit).toBeTruthy();
+  });
 });
-//test the handlesubmit to test functionality
